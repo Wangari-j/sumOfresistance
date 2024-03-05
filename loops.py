a75a26c7b90889be831d4f1e90b87189b7d004e7
Wangari-j <wangarijudy@gmail.com>
2024-03-05 22:54:47 +0300

    loops

--- loops.py
+++ loops.py
@@ -1,7 +1,17 @@
-def seriesResistance(RT1,RT2,RT3,RT4):
+def seriesResistance(numbers):
+    sum = 0
 
-    for i in seriesResistance:
-     sum = RT1 + RT2 + RT3 + RT4
-     return sum
+    for i in numbers:
+        sum = i + sum
 
-seriesResistance(1,5,6,3)
+    return sum
+
+nums = [1,5,6,3]
+numb = [10,30,45,15,5]
+j = [ 16,3.5, 6]
+k = [0.5, 0.5]
+
+print(seriesResistance(nums))
+print(seriesResistance(numb))
+print(seriesResistance(j))
+print(seriesResistance(k))
